09b3f747c6ab9a5eaa93d59fff200dc63ed9d104
Ionel14 <53469111+Ionel14@users.noreply.github.com>
2023-05-31 10:27:37 +0300

    findUser created

--- Project/app/src/main/java/com/example/findmyteam/data/UsersManagement.java
+++ Project/app/src/main/java/com/example/findmyteam/data/UsersManagement.java
@@ -4,6 +4,7 @@ import static com.example.findmyteam.helpers.Constants.BASE_URL;
 import static com.example.findmyteam.helpers.Constants.USERS_ENDPOINT;
 import static java.security.AccessController.getContext;
 
+import android.content.Context;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -11,53 +12,61 @@ import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.example.findmyteam.helpers.VolleyConfigSingleton;
+import com.example.findmyteam.models.User;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.concurrent.CompletableFuture;
+
 public class UsersManagement {
-//    private void getUsers() {
-//        VolleyConfigSingleton volleyConfigSingleton = VolleyConfigSingleton.getInstance(getContext());
-//        RequestQueue queue = volleyConfigSingleton.getRequestQueue();
-//        String url = BASE_URL + USERS_ENDPOINT;
-//
-//        // Create a JSONObject containing the email and password
-//        JSONObject jsonBody = new JSONObject();
-//        try {
-//            jsonBody.put("email", "ew");
-//            jsonBody.put("password", "d");
-//        } catch (JSONException e) {
-//            e.printStackTrace();
-//        }
-//
-//        JsonObjectRequest getUsersRequest = new JsonObjectRequest(
-//                Request.Method.POST,
-//                url,
-//                jsonBody,
-//                response -> {
-//                    // Handle the response from the server
-//                    try {
-//                        // Extract user data from the response JSON
-//                        String username = response.getString("username");
-//                    } catch (JSONException e) {
-//                        Toast.makeText(
-//                                getContext(),
-//                                "ERROR: Failed to parse response",
-//                                Toast.LENGTH_SHORT
-//                        ).show();
-//                    }
-//                    hideLoading();
-//                },
-//                error -> {
-//                    // Handle the error response
-//                    Toast.makeText(
-//                            getContext(),
-//                            "ERROR: " + error.getMessage(),
-//                            Toast.LENGTH_SHORT
-//                    ).show();
-//                    hideLoading();
-//                }
-//        );
-//        queue.add(getUsersRequest);
-//    }
+    private CompletableFuture<User> findUser(Context context, String email) {
+        CompletableFuture<User> completableFuture = new CompletableFuture<>();
+
+        VolleyConfigSingleton volleyConfigSingleton = VolleyConfigSingleton.getInstance(context);
+        RequestQueue queue = volleyConfigSingleton.getRequestQueue();
+        String url = BASE_URL + USERS_ENDPOINT;
+
+        JsonObjectRequest getUsersRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                response -> {
+                    try {
+                        JSONArray usersArray = response.getJSONArray("users");
+
+                        for (int i = 0; i < usersArray.length(); i++) {
+                            JSONObject userObj = usersArray.getJSONObject(i);
+
+                            String emailU = userObj.getString("email");
+                            if (emailU.equals(email)) {
+                                String password = userObj.getString("password");
+                                User currentUser = new User(email, password);
+                                completableFuture.complete(currentUser);
+                                return; // Exit the loop and function since user is found
+                            }
+                        }
+
+                        // User not found
+                        completableFuture.complete(null);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        // Handle the error response
+                        completableFuture.completeExceptionally(e);
+                    }
+                },
+                error -> {
+                    error.printStackTrace();
+                    // Handle the error response
+                    completableFuture.completeExceptionally(error);
+                }
+        );
+
+        queue.add(getUsersRequest);
+
+        return completableFuture;
+    }
+
+
 }
