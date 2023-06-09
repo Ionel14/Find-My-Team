6cb0564f9ec1c21ea66cad865a6321515e53f21e
Catalinul3 <115569824+Catalinul3@users.noreply.github.com>
2023-06-09 13:37:41 +0300

    Create second activity for announce app (instabil)

---
+++ Project/app/src/main/java/com/example/findmyteam/activities/AppMain.kt
@@ -0,0 +1,12 @@
+package com.example.findmyteam.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.findmyteam.R
+
+class AppMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app_main)
+    }
+}
